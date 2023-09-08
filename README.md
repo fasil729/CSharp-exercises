da0cf1082411d858f11ed7ae26d9dc349919c850
fasika <fasikafikadu38@gmail.com>
2023-09-08 20:45:28 +0300

    add student average grade calculator

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # C-exercises
 c# exericeses at A2SV
+
+# Student Grade Calculator
+
+This is a C# console application that allows students to calculate their average grade based on different subjects.
+
+## Features
+
+- Prompt the student to enter their name and the number of subjects they have taken.
+- For each subject, the student can enter the subject name and the grade obtained (numeric value).
+- Validate input using conditional statements to ensure grade values are within a valid range.
+- Use loops to handle multiple subjects and grades.
+- Utilize collections (such as List or Dictionary) to store subject names and corresponding grades.
+- Define a method to calculate the average grade based on the entered grades.
+- Display the student's name, individual subject grades, and the calculated average grade using string interpolation.
+
+
+## Example
+
+```plaintext
+Welcome to the Student Grade Calculator!
+
+Please enter your name: John Doe
+Please enter the number of subjects: 3
+
+Subject 1 Name: Mathematics
+Grade: 85
+
+Subject 2 Name: Science
+Grade: 92
+
+Subject 3 Name: English
+Grade: 78
+
+Calculating average grade...
+
+Student: John Doe
+
+Subject Grades:
+- Mathematics: 85
+- Science: 92
+- English: 78
+
+Average Grade: 85
+
+Thank you for using the Student Grade Calculator!
---
+++ Student Grade Calculator/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+// take input
+Console.WriteLine("Welcome to the Student Grade Calculator!");
+
+Console.WriteLine("Please enter your name: ");
+string name = Console.ReadLine();
+Console.WriteLine("Please enter the number of subjects: ");
+string s = Console.ReadLine();
+int sub = int.Parse(s);
+Dictionary<string, int> subjects = new Dictionary<string, int>();
+for (int i = 1; i <= sub; i++) 
+{
+   Console.WriteLine($"Subject {i} Name: "); 
+   string subject = Console.ReadLine();
+   Console.WriteLine("Grade: "); 
+   int grade = int.Parse(Console.ReadLine());
+   if (grade >= 0 && grade <= 100) 
+   {
+    subjects[subject] = grade;
+
+   }
+   else {
+      Console.WriteLine("In correct Input!!! Please Enter the grade beteween 0 and 100 !!!");
+      i--; 
+   }
+}
+
+// printing the output
+Console.WriteLine("");
+Console.WriteLine("Calculating average grade...");
+Console.WriteLine("");
+Console.WriteLine($"Student: {name}");
+Console.WriteLine("");
+Console.WriteLine("Subject Grades: ");
+
+foreach (KeyValuePair<string, int> pair in subjects)
+{
+
+    Console.WriteLine($"- {pair.Key}: {pair.Value}");
+}
+
+Console.WriteLine("");
+
+
+Console.WriteLine($"Average Grade: {getAverage(subjects)}");
+
+Console.WriteLine("");
+Console.WriteLine("Thank you for using the Student Grade Calculator!");
+
+
+
+int getAverage(Dictionary<string, int> subjects)
+{
+    int tot = 0;
+    int num = 0;
+    foreach (KeyValuePair<string, int> pair in subjects)
+    {
+
+        tot += pair.Value;
+        num++;
+    }
+    return tot / num;
+}
+
